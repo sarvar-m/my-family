920c65ff893784ca8c6b7b72dafb0bc6706932f1
Sarvar <sarvar.m149@gmail.com>
2021-07-21 12:37:26 +0700

    starting the server

--- server/express.js
+++ server/express.js
@@ -7,11 +7,11 @@ import helmet from "helmet";
 
 const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true})
-app.use(cookieParser())
-app.use(compression())
-app.use(helmet())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
+app.use(cors());
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,36 +1,46 @@
-import path from "path";
-import express from "express";
-import { MongoClient } from "mongodb";
-import template from "./../template";
-//comment out before building for production
-import devBundle from "./devBundle";
+// import path from "path";
+// import express from "express";
+// import { MongoClient } from "mongodb";
+// import template from "./../template";
+// //comment out before building for production
+// import devBundle from "./devBundle";
 
-const app = express();
-//comment out before building for production
-devBundle.compile(app);
+// const app = express();
+// //comment out before building for production
+// devBundle.compile(app);
 
-const CURRENT_WORKING_DIR = process.cwd();
-app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+// const CURRENT_WORKING_DIR = process.cwd();
+// app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
 
-app.get("/", (req, res) => {
-  res.status(200).send(template());
-});
+// app.get("/", (req, res) => {
+//   res.status(200).send(template());
+// });
 
-let port = process.env.PORT || 3000;
-app.listen(port, function onStart(err) {
+// let port = process.env.PORT || 3000;
+// app.listen(port, function onStart(err) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.info("Server started on port %s.", port);
+// });
+
+// const url =
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/mernSimpleSetup";
+// MongoClient.connect(
+//   url,
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   (err, db) => {
+//     console.log("Connected successfully to mongodb server");
+//     // db.close();
+//   }
+// );
+
+import config from "./../config/config";
+import app from "./express";
+
+app.listen(config.port, (err) => {
   if (err) {
     console.log(err);
   }
-  console.info("Server started on port %s.", port);
+  console.info("Server started on port %s.", config.port);
 });
-
-const url =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/mernSimpleSetup";
-MongoClient.connect(
-  url,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, db) => {
-    console.log("Connected successfully to mongodb server");
-    // db.close();
-  }
-);
